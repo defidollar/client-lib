6487b6944664b88a669e4542b1a9af56f9dfbe26
Manthankumar Satani <satanimanthan@gmail.com>
2021-04-09 00:49:11 +0530

    refactor: defidollar client for bsc and new abis

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
 module.exports = {
   DefiDollarClient: require("./clients/DefiDollarClient"),
-  ILMOClient: require("./clients/ILMOClient"),
   SavingsClient: require("./clients/SavingsClient"),
-  ibDFDClient: require("./clients/ibDFDClient"),
-  LPRewards: {
-    SnxStyleRewards: require("./clients/lpRewards/SnxStyleRewards"),
-    BalDfdDusdClient: require("./clients/lpRewards/BalDfdDusd"),
-    VestingClient: require("./clients/lpRewards/Vesting")
-  }
 };
--- minify.js
+++ minify.js
@@ -1,24 +1,20 @@
-const fs = require('fs');
+const fs = require("fs");
 
 const contracts = [
-  'ERC20Detailed.json',
-  'YVaultPeak.json',
-  'YVaultZap.json',
-  'StakeLPToken.json',
-  'Core.json',
-  'DFDRewards.json',
-  'TokenVesting.json',
-  'ibDUSD.json',
-  'ibDFD.json'
-]
+  "ERC20Detailed.json",
+  "YVaultPeak.json",
+  "YVaultZap.json",
+  "Core.json",
+  "ibDUSD.json",
+];
 
-const artifacts = fs.readdirSync(`artifacts`)
-  artifacts.forEach(a => {
-    const name = `artifacts/${a}`
-    const abi = JSON.parse(fs.readFileSync(name)).abi
-    if (!abi.length || !contracts.includes(a)) {
-      fs.unlinkSync(name)
-    } else {
-      fs.writeFileSync(name, JSON.stringify({ abi }) + '\n')
-    }
-})
+const artifacts = fs.readdirSync(`artifacts`);
+artifacts.forEach((a) => {
+  const name = `artifacts/${a}`;
+  const abi = JSON.parse(fs.readFileSync(name)).abi;
+  if (!abi.length || !contracts.includes(a)) {
+    fs.unlinkSync(name);
+  } else {
+    fs.writeFileSync(name, JSON.stringify({ abi }) + "\n");
+  }
+});
