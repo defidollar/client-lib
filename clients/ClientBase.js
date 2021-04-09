0f5b2b3c743687d24e1eb69e71a4fc40bd81312c
Arpit Agarwal <93arpit@gmail.com>
2021-04-09 12:36:51 +0530

    New zap - mintAndDeposit (#8)
    * mintAndDeposit
    
    * _sanitizeTokens in clientBase
    
    * fix: importing ClientBase
    
    * chg: adding editor config and eslintrc to avoid formatting conflicts
    
    * fix: import ClientBase
    
    * fix: import ClientBase
    
    * chg: linting and const import fix ClientBase
    
    * fix: assert import
    
    Co-authored-by: Manthankumar Satani <satanimanthan@gmail.com>

---
+++ clients/ClientBase.js
@@ -0,0 +1,63 @@
+const assert = require("assert").strict;
+const Web3 = require("web3");
+
+const toBN = Web3.utils.toBN;
+const toWei = Web3.utils.toWei;
+const TEN_THOUSAND = toBN(10000);
+
+class ClientBase {
+  constructor(config) {
+    this.config = config;
+    this.yVaultPeak = config.contracts.peaks.yVaultPeak;
+  }
+
+  adjustForSlippage(amount, decimals, slippage) {
+    slippage = parseFloat(slippage);
+    if (isNaN(slippage) || slippage < 0 || slippage > 100) {
+      throw new Error(`Invalid slippage value: ${slippage} provided`);
+    }
+    amount = decimals ? this.scale(amount, decimals) : toBN(amount);
+    if (amount.eq(toBN(0)) || slippage == 0) return amount.toString();
+    return toBN(amount)
+      .mul(TEN_THOUSAND.sub(toBN(parseFloat(slippage) * 100)))
+      .div(TEN_THOUSAND)
+      .toString();
+  }
+
+  scale(num, decimals) {
+    num = toBN(toWei(num.toString()));
+    if (decimals < 18) {
+      num = num.div(toBN(10).pow(toBN(18 - decimals)));
+    } else if (decimals > 18) {
+      num = num.mul(toBN(10).pow(toBN(decimals - 18)));
+    }
+    return num;
+  }
+
+  _processAmounts(tokens) {
+    Object.keys(tokens).forEach((t) =>
+      assert.ok(this.yVaultPeak.coins.includes(t), "bad coins")
+    );
+    const inAmounts = new Array(4);
+    for (let i = 0; i < this.yVaultPeak.coins.length; i++) {
+      const c = this.yVaultPeak.coins[i];
+      if (tokens[c]) {
+        inAmounts[i] = this.scale(
+          tokens[c],
+          this.config.contracts.tokens[c].decimals
+        ).toString();
+      } else {
+        inAmounts[i] = 0;
+      }
+    }
+    return inAmounts;
+  }
+
+  _sanitizeTokens(tokens) {
+    Object.keys(tokens).forEach((t) => {
+      if (!tokens[t] || isNaN(parseFloat(tokens[t]))) delete tokens[t];
+    });
+  }
+}
+
+module.exports = ClientBase;
--- clients/SavingsClient.js
+++ clients/SavingsClient.js
@@ -1,80 +1,124 @@
-const Web3 = require('web3')
+const Web3 = require("web3");
 
-const Web3Client = require('./Web3Client')
+const Web3Client = require("./Web3Client");
 
-const ibDUSD = require('../artifacts/ibDUSD.json')
-const IERC20 = require('../artifacts/ERC20Detailed.json')
-const utils = require('./utils')
-const { toBN, toWei, fromWei } = utils
+const ibDUSD = require("../artifacts/ibDUSD.json");
+const IERC20 = require("../artifacts/ERC20Detailed.json");
+const zap = require("../artifacts/YVaultZap.json");
 
-class SavingsClient {
-    constructor(web3, config) {
-        web3 = web3 || new Web3()
-        this.web3Client = new Web3Client(web3)
-        this.config = config
-        this.ibDusd = new web3.eth.Contract(ibDUSD.abi, config.contracts.tokens.ibDUSD.address)
-        this.dusd = new web3.eth.Contract(IERC20.abi, config.contracts.tokens.DUSD.address)
+const utils = require("./utils");
+
+const ClientBase = require("./ClientBase.js");
+
+const { toBN, toWei, fromWei } = utils;
+
+class SavingsClient extends ClientBase {
+  constructor(web3, config) {
+    super(config);
+    web3 = web3 || new Web3();
+    this.web3Client = new Web3Client(web3);
+    this.ibDusd = new web3.eth.Contract(
+      ibDUSD.abi,
+      config.contracts.tokens.ibDUSD.address
+    );
+    this.dusd = new web3.eth.Contract(
+      IERC20.abi,
+      config.contracts.tokens.DUSD.address
+    );
+    this.zap = new web3.eth.Contract(
+      zap.abi,
+      this.config.contracts.peaks.yVaultPeak.zap
+    );
+  }
+
+  /**
+   * @param {*} amount # DUSD to deposit
+   * @param {*} options Tx options
+   */
+  deposit(amount, options = {}) {
+    const txObject = this.ibDusd.methods.deposit(toWei(amount.toString()));
+    return this.web3Client.send(txObject, options);
+  }
+
+  /**
+   * @notice Mint DUSD and deposit in savings vault
+   * @dev Don't send values scaled with decimals. The following code will handle it.
+   * @param {*} tokens InAmounts in the format { DAI: '6.1', USDT: '0.2', ... }
+   * @param {*} dusdAmount Expected dusd amount not accounting for the slippage
+   * @param {*} slippage Maximum allowable slippage 0 <= slippage <= 100 %
+   */
+  mintAndDeposit(tokens, dusdAmount, slippage, options = {}) {
+    this._sanitizeTokens(tokens);
+    const minDusdAmount = this.adjustForSlippage(
+      dusdAmount,
+      18,
+      slippage
+    ).toString();
+    const txObject = this.zap.methods.deposit(
+      this._processAmounts(tokens),
+      minDusdAmount
+    );
+    return this.web3Client.send(txObject, options);
+  }
+
+  /**
+   * @param {*} amount ~ # of DUSD to withdraw - NOT Shares. Will be ignored if isMax is provided.
+   * @param {*} isMax Whether the user opted to exit completely
+   * @param {*} options Tx options
+   */
+  async withdraw(amount, isMax, options = {}) {
+    let shares;
+    if (isMax) {
+      shares = await this.ibDusd.methods.balanceOf(options.from).call();
+    } else {
+      shares = utils
+        .scale(amount, 36)
+        .div(toBN(await this.ibDusd.methods.getPricePerFullShare().call()));
     }
+    const txObject = this.ibDusd.methods.withdraw(shares.toString());
+    return this.web3Client.send(txObject, options);
+  }
 
-    /**
-     * @param {*} amount # DUSD to deposit
-     * @param {*} options Tx options
-     */
-    deposit(amount, options = {}) {
-        const txObject = this.ibDusd.methods.deposit(toWei(amount.toString()))
-        return this.web3Client.send(txObject, options)
-    }
+  /**
+   * @notice ibDusd and DUSD Balance
+   * @return { ibDusd, dusd, withrawable } in wei
+   * ibDusd - ibDusd balance
+   * withrawable - dusd withrawable from the savings ibDusd contract
+   * dusd - dusd wallet balance
+   */
+  async balanceOf(account) {
+    const [ibDusd, pricePerFullShare, dusd] = await Promise.all([
+      this.ibDusd.methods.balanceOf(account).call(),
+      this.ibDusd.methods.getPricePerFullShare().call(),
+      this.dusd.methods.balanceOf(account).call(),
+    ]);
+    return {
+      ibDusd,
+      withrawable: fromWei(toBN(ibDusd).mul(toBN(pricePerFullShare))),
+      dusd,
+    };
+  }
 
-    /**
-     * @param {*} amount ~ # of DUSD to withdraw - NOT Shares. Will be ignored if isMax is provided.
-     * @param {*} isMax Whether the user opted to exit completely
-     * @param {*} options Tx options
-     */
-    async withdraw(amount, isMax, options = {}) {
-        let shares
-        if (isMax) {
-            shares = await this.ibDusd.methods.balanceOf(options.from).call()
-        } else {
-            shares = utils.scale(amount, 36).div(toBN(await this.ibDusd.methods.getPricePerFullShare().call()))
-        }
-        const txObject = this.ibDusd.methods.withdraw(shares.toString())
-        return this.web3Client.send(txObject, options)
-    }
+  /**
+   * @notice DUSD allowance for the savings contract
+   */
+  allowance(account) {
+    return this.dusd.methods
+      .allowance(account, this.ibDusd.options.address)
+      .call();
+  }
 
-    /**
-     * @notice ibDusd and DUSD Balance
-     * @return { ibDusd, dusd, withrawable } in wei
-     * ibDusd - ibDusd balance
-     * withrawable - dusd withrawable from the savings ibDusd contract
-     * dusd - dusd wallet balance
-     */
-    async balanceOf(account) {
-        const [ibDusd, pricePerFullShare, dusd] = await Promise.all([
-            this.ibDusd.methods.balanceOf(account).call(),
-            this.ibDusd.methods.getPricePerFullShare().call(),
-            this.dusd.methods.balanceOf(account).call()
-        ])
-        return { ibDusd, withrawable: fromWei(toBN(ibDusd).mul(toBN(pricePerFullShare))), dusd }
-    }
-
-    /**
-     * @notice DUSD allowance for the savings contract
-     */
-    allowance(account) {
-        return this.dusd.methods.allowance(account, this.ibDusd.options.address).call()
-    }
-
-    /**
-     * @notice approve
-     * @param amount Amount without having accounted for decimals
-     */
-    approve(amount, options = {}, trust = false) {
-        const txObject = this.dusd.methods.approve(
-            this.ibDusd.options.address,
-            trust ? amount : toWei(amount.toString())
-        )
-        return this.web3Client.send(txObject, options)
-    }
+  /**
+   * @notice approve
+   * @param amount Amount without having accounted for decimals
+   */
+  approve(amount, options = {}, trust = false) {
+    const txObject = this.dusd.methods.approve(
+      this.ibDusd.options.address,
+      trust ? amount : toWei(amount.toString())
+    );
+    return this.web3Client.send(txObject, options);
+  }
 }
 
-module.exports = SavingsClient
+module.exports = SavingsClient;
